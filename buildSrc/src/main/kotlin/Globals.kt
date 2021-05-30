2408da85eeb84ddcee6ea738f86197ddbd859185
BoD <BoD@JRAF.org>
2021-05-30 23:07:55 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/Globals.kt
+++ buildSrc/src/main/kotlin/Globals.kt
@@ -1,8 +1,8 @@
 object Versions {
     // Misc and plugins
     const val GRADLE = "7.0.2"
-    const val KOTLIN = "1.5.0"
-    const val BEN_MANES_VERSIONS_PLUGIN = "0.38.0"
+    const val KOTLIN = "1.5.10"
+    const val BEN_MANES_VERSIONS_PLUGIN = "0.39.0"
     const val DOKKA_PLUGIN = "1.4.32"
 
     // Lib dependencies
