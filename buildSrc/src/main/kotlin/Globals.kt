fd300047d0a523fabf2fbce7023d9ff3f4b41366
BoD <BoD@JRAF.org>
2021-05-31 23:04:07 +0200

    Update to KTOR 1.6.0

--- buildSrc/src/main/kotlin/Globals.kt
+++ buildSrc/src/main/kotlin/Globals.kt
@@ -7,7 +7,7 @@ object Versions {
 
     // Lib dependencies
     const val KOTLIN_SERIALIZATION = KOTLIN
-    const val KTOR = "1.5.4"
+    const val KTOR = "1.6.0"
     const val COROUTINES = "1.5.0"
     const val SLF4J = "1.7.30"
     const val KOTLINX_CLI = "0.3.2"
