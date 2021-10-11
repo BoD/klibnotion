ec2858617b4ea4fd1233f4ae5451406929bb86db
Yu Jinyan <yujinyan@outlook.com>
2021-10-11 23:41:10 +0800

    Support writing bookmark, code, equation, callout, quote, embed blocks

--- library/src/commonMain/kotlin/org/jraf/klibnotion/internal/api/model/block/ApiOutBlockConverter.kt
+++ library/src/commonMain/kotlin/org/jraf/klibnotion/internal/api/model/block/ApiOutBlockConverter.kt
@@ -31,16 +31,22 @@ import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 import kotlinx.serialization.json.putJsonObject
 import org.jraf.klibnotion.internal.api.model.ApiConverter
+import org.jraf.klibnotion.internal.api.model.base.ApiOutEmojiOrFileConverter
 import org.jraf.klibnotion.internal.api.model.modelToApi
 import org.jraf.klibnotion.internal.api.model.richtext.ApiOutRichTextListConverter
 import org.jraf.klibnotion.model.block.Block
+import org.jraf.klibnotion.model.block.BookmarkBlock
 import org.jraf.klibnotion.model.block.BulletedListItemBlock
+import org.jraf.klibnotion.model.block.CalloutBlock
 import org.jraf.klibnotion.model.block.CodeBlock
+import org.jraf.klibnotion.model.block.EmbedBlock
+import org.jraf.klibnotion.model.block.EquationBlock
 import org.jraf.klibnotion.model.block.Heading1Block
 import org.jraf.klibnotion.model.block.Heading2Block
 import org.jraf.klibnotion.model.block.Heading3Block
 import org.jraf.klibnotion.model.block.NumberedListItemBlock
 import org.jraf.klibnotion.model.block.ParagraphBlock
+import org.jraf.klibnotion.model.block.QuoteBlock
 import org.jraf.klibnotion.model.block.ToDoBlock
 import org.jraf.klibnotion.model.block.ToggleBlock
 import org.jraf.klibnotion.model.richtext.RichTextList
@@ -61,7 +67,11 @@ internal object ApiOutBlockConverter : ApiConverter<JsonElement, Block>() {
                 is ToDoBlock -> "to_do"
                 is ToggleBlock -> "toggle"
                 is CodeBlock -> "code"
-
+                is BookmarkBlock -> "bookmark"
+                is EquationBlock -> "equation"
+                is QuoteBlock -> "quote"
+                is EmbedBlock -> "embed"
+                is CalloutBlock -> "callout"
                 else -> throw IllegalStateException()
             }
             put("type", type)
@@ -70,6 +80,17 @@ internal object ApiOutBlockConverter : ApiConverter<JsonElement, Block>() {
                 when (model) {
                     is ToDoBlock -> put("checked", model.checked)
                     is CodeBlock -> put("language", model.language)
+                    is BookmarkBlock -> {
+                        put("url", model.url)
+                        model.caption?.let {
+                            put("caption", it.modelToApi(ApiOutRichTextListConverter))
+                        }
+                    }
+                    is EquationBlock -> put("expression", model.expression)
+                    is EmbedBlock -> put("url", model.url)
+                    is CalloutBlock -> model.icon?.let {
+                        put("icon", it.modelToApi(ApiOutEmojiOrFileConverter))
+                    }
                     else -> {
                     }
                 }
